ecfbb0793c70bbf1d5fad18fb35e453874e74145
danyRafina <danyrafina@gmail.com>
2014-09-29 16:58:39 +0200

    commit Revision[attr.pie] Revision[getPie()]

--- rentacoRev/src/Revision.java
+++ rentacoRev/src/Revision.java
@@ -1,10 +1,17 @@
 class Revision {
 	private String im ;
 	private int numero ;
+	public static float pie;
 	
 	public Revision(String im, int numero){
 		this.setIm(im);
 		this.numero = numero;
+	}
+	public static float getPie() {
+		return pie;
+	}
+	public static void setPie(float pie) {
+		Revision.pie = pie;
 	}
 	public int getNumero(){
 		return this.numero;
